01fdee45dcf42f0b94d169e62cd542f30eea3f36
James Swineson <github@public.swineson.me>
2018-07-02 10:25:36 +0800

    more config

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,16 @@ if grep -q 1 /proc/sys/net/ipv6/conf/all/disable_ipv6; then
     echo "Warning: IPv6 is not available in container." >&2
 fi
 
+echo "Bootstraping environment..."
+
 # populate DNS root key if already available as package
 if [ -f /usr/share/dns/root.key ]; then
     echo "Populating DNS root key..."
     cp -f /usr/share/dns/root.key /config/knot-resolver/root.key
 fi
+
+# try populate root hints
+curl -sSL https://www.internic.net/domain/named.root -O /config/knot-resolver/named.root
 
 echo "" > /etc/resolv.conf
 echo "DNS configuration for bootstrap:"
