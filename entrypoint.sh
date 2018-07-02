13f0975202b2fe0a205063c6072b1403dcacd246
James Swineson <github@public.swineson.me>
2018-07-02 09:20:05 +0800

    check IPv6 availibility in container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 
+# check if IPv6 is available in container
+if grep -q 1 /proc/sys/net/ipv6/conf/all/disable_ipv6; then
+    echo "Warning: IPv6 is not available in container." >&2
+fi
+
 echo "" > /etc/resolv.conf
 echo "DNS configuration for bootstrap:"
 echo $BOOTSTRAP_DNS_SERVER | sed -e "s/,/\n/g" | sed -e "s/^/nameserver /g" | tee /etc/resolv.conf
