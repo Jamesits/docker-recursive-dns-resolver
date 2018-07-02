5ae5a97cf4b7036bbc471187165224bd93a7a0ab
James Swineson <github@public.swineson.me>
2018-07-02 08:55:21 +0800

    do not directly delete resolv.conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-rm -f /etc/resolv.conf
+echo "" > /etc/resolv.conf
 echo "DNS configuration for bootstrap:"
 echo $BOOTSTRAP_DNS_SERVER | sed -e "s/,/\n/g" | sed -e "s/^/nameserver /g" | tee /etc/resolv.conf
 
